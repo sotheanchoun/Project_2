490f22ffc217247f6a009feb9e08564f22c55180
Student <student@PNCLB13-14.pnc.lan>
2021-05-20 22:34:46 +0700

    f1-f2 not already

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Hello Worl");
+function sendMessage(){
+    let message = document.getElementById('typing-box').value;
+    let html = '<div class="message-box my-message-box">'+ 
+    '<div class="message my-message">' + message + '</div>'
+     + '<div class="separator"></div>' + '</div>';
+    
+    document.getElementById('message-area').innerHTML += html;
+
+    document.getElementById('typing-box').value ="";
+}
