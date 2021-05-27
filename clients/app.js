c9f7715c16981a25168e7b0adccaa0237fe81e88
sophannamey <sophanna.mey@student.passerellesnumeriques.org>
2021-05-27 20:31:41 +0700

    add users  on server

--- clients/app.js
+++ clients/app.js
@@ -1,16 +1,81 @@
-console.log("Hello Worl");
-function sendMessage(){
-    let message = document.getElementById('typing-box').value;
-    let html =
-     '<div class="message-box my-message-box">'+ 
-        '<div class="my-photo">'+'</div>'+
-        '<div class="message my-message">' +
-             message +
-        '</div>'+
-        '<div class="separator"></div>' +
-     '</div>';
-   
-    document.getElementById('message-area').innerHTML += html;
 
-    document.getElementById('typing-box').value ="";
+// function sendMessage(){
+    
+// }
+
+function displayInfo(res){
+    const myMessage = document.querySelector('.message-box');
+    let users = res.data;
+    let list = document.querySelector('ul');
+    if (list !== null) list.remove();
+    console.log(users)
+    const Message = document.createElement('div');
+    Message.className = 't';
+    const ul = document.createElement('ul');
+
+
+    for (let user of users){
+        let li = document.createElement('li');
+        li.className = "myMessage";
+        li.textContent = user.text;
+       
+
+        Message.appendChild(ul);
+        ul.appendChild(li);
+     
+    }
+    myMessage.appendChild(Message);
+    Message.appendChild(ul);
 }
+function addText() {
+    const text = document.querySelector('#typing-box').value;
+    let user = {name:"unknow" , text: text};
+    axios.post(url,user).then(displayInfo);
+}
+
+
+
+const url = "http://localhost:5000/users";
+axios.get(url).then(displayInfo)
+
+
+const btnSend = document.querySelector('button');
+btnSend.addEventListener('click',addText);
+
+
+
+
+
+// function displayInfo(res) {
+//     let users = res.data;
+//     let list = document.querySelector('ul');
+//     let messageArea = document.createElement('div');
+//     if (list !== null ) list.remove();
+//     console.log(users);
+//     const ul = document.querySelector('ul');
+//     for (let user of users){
+//         let li = document.createElement('li');
+//         li.textContent = "@" + user.name + " : " + user.text;
+//         console.log(li)
+//         ul.appendChild(li);
+//         messageArea.appendChild(ul).textContent;
+//         document.body.appendChild(messageArea)
+        
+//     }
+    
+   
+// }
+
+// function addText(){
+//     const text = document.querySelector('#text').value;
+//     let user = {name: "unknown", text: text}
+//     console.log(user);
+//     axios.post(url,user).then(displayInfo);
+// }
+
+// const url = "http://localhost:5000/users";
+// axios.get(url).then(displayInfo);
+
+// const btnSend = document.querySelector('button');
+// btnSend.addEventListener('click',addText);
+
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.listen(process.env.PORT || 5000, () => console.log("Running..."))
+app.listen(process.env.PORT || 5000, () => console.log("Running..."));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static("clients"));
+
+let users = [
+    {id: 1,name: "rady",text: "Hello world"},
+    // {id: 2,name: "dara",text: "Hello world"},
+    // {id: 3,name: "vira",text: "Hello world"},
+]
+
+app.get('/users',(req,res)=> res.send(users))
+
+app.post('/users',(req,res)=> {
+    let username = req.query.name;
+    let texts = req.body.text;
+    let user = {
+        id: users.length + 1,
+        name: username,
+        text: texts,
+    }
+    users.push(user);
+    res.send(users);
+})
